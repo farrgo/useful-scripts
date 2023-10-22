d13d3140f9bed0010369d8a62a3cc1cf49d4e063
David <david@davidfarr.ca>
2023-10-22 11:35:47 -0400

    Activated line to empty Prefetch folder.

--- delete-temporary-files.bat
+++ delete-temporary-files.bat
@@ -1,4 +1,4 @@
 :: Use an centralized folder-emptying script to empty each temporary folder.
 call delete-folder-contents.bat %TEMP%
 call delete-folder-contents.bat "%WINDIR%\Temp"
-::call delete-folder-contents.bat "%WINDIR%\Prefetch"
+call delete-folder-contents.bat "%WINDIR%\Prefetch"
