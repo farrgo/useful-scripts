d4748c010fe178ff9fac2aeacd8d6d9b3ecb96b6
David Farr <50378911+farrgo@users.noreply.github.com>
2023-10-27 12:03:42 -0400

    Re-wrote script in Power Shell
    Script is now in Power Shell to be more functional. It has logging and reads the list of folders to delete from a text file.

---
+++ delete-temporary-files.ps1
@@ -0,0 +1,69 @@
+# This script reads in a list of folders containing temporary files and attempts to delete their contents.
+
+# This function takes the path of a folder as its parameter and attempts to delete it.
+function Remove-EverythingInFolder {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$FolderPath
+    )
+
+    Write-Output "Attempting to empty folder $FolderPath."
+    
+    Write-Verbose -Message "Checking if folder $FolderPath exists."
+    if (Test-Path -Path $FolderPath) {
+        
+        Write-Verbose -Message "The folder $FolderPath exists."
+
+        Write-Verbose -Message "Checking if $FolderPath contains this script, which we don't want to delete!"
+        if ($FolderPath.ToUpper() -eq $PSScriptRoot.ToUpper()) {
+
+            Write-Output "ERROR: The folder $FolderPath contains this script and cannot be emptied without moving the script!"
+
+        } else {
+
+            Write-Verbose -Message "The folder $FolderPath does not contain this script, so we will proceed."
+
+            Write-Verbose -Message "Deleting all files and folders in $FolderPath."
+            Remove-Item $FolderPath\* -Recurse -Force
+
+            Write-Verbose -Message "Checking how many items are still in the folder to tell if it's empty."
+            
+            # Get the number of items left in the folder.
+            [int32]$NumberItemsInFolder = (Get-ChildItem $FolderPath | Measure-Object).Count
+            
+            # Check if it's zero.
+            if ($NumberItemsInFolder -eq 0) {
+                
+                Write-Output "Folder $FolderPath has been emptied."
+            
+            } else {
+
+                Write-Output "Folder $FolderPath has $NumberItemsInFolder items remaining. Check if you have permission to empty it."
+
+            }         
+
+        }
+
+
+    } else {
+
+        Write-Output "ERROR: The folder $FolderPath does not exist!"
+
+    }
+
+}
+
+Write-Verbose -Message "Reading the list of folders to empty into a array."
+$Folders_To_Empty = Get-Content "Folders_To_Empty.txt" | Where-Object { $_.Trim() -ne '' }
+
+Write-Verbose -Message "Trying to empty each folder in the array."
+foreach ($Folder in $Folders_To_Empty) {
+    
+    Remove-EverythingInFolder $Folder
+
+}
+
+Write-Verbose -Message "Trying to empty user's temporary folder."
+Remove-EverythingInFolder $Env:TEMP
